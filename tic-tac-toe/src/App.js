eecdd3d6d704bb6160ec18d7d5b13e0c5594498b
Parth Vekariya <parthvekariya260898@gmail.com>
2023-12-15 20:55:52 -0500

    player components with dynamic values

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Player from './components/Player';
 
 function App() {
   return (
-    <div className="App">
-      <p>Tic-Tac-Toe</p>
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
   );
 }
 
